d4bfcc928e1e16b0238ebeee0f2a2290e7c9963d
Rosnel Alejandro Leyva-Cortes <30304057+Rosnel14@users.noreply.github.com>
2021-08-04 08:50:45 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
-phone_number = "phone number"
-message = ["Good morning babe", "Good morning my love", "Good morning cutie"]
-scheduled_time = "08:00"
+phone_number = "+12019529990"
+message = ["Hello, Dino_bot here! I'm here to wake you up with love because Alejandro is sleeping. You are beautiful and loved! Go seize the day.", "Hello, Dino_bot here! I'm here to wake you up with love because Alejandro is sleeping. You are smart and amazing, future doctor here we go! ", "Good morning cutie! Dino_bot here! I'm here to wake you up with love because Alejandro is sleeping. Believe in yourself, you are your greatest weapon! :)"]
+scheduled_time = "08:50"
